e9fcc8dfce4369aec6c04990b9b2880d45e79e4c
Miguel Angel <miguelangelpabonherrera@gmail.com>
2024-11-16 14:40:06 -0500

    Subimos la calculadora para practicar git merge

---
+++ script.js
@@ -0,0 +1,14 @@
+function agregar(value) {
+    document.getElementById('pantalla').value += value;
+  }
+  
+  function calcular() {
+    const valorPantalla = document.getElementById('pantalla').value;
+    const result = eval(valorPantalla);
+    document.getElementById('pantalla').value = result;
+  }
+  
+  function limpiar() {
+    document.getElementById('pantalla').value = '';
+  }
+  
